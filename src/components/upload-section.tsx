d72052e457649450ff22af30a1a4573ed70ed5de
ozankersit <ozankersit@gmail.com>
2025-01-14 22:12:27 +0300

    request refactoring

--- src/components/upload-section.tsx
+++ src/components/upload-section.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef } from "react";
+import { useState, useRef, useTransition } from "react";
 
 import type { PutBlobResult } from "@vercel/blob";
 import UploadContainer from "./upload-container";
@@ -9,48 +9,46 @@ import BlobResultSection from "./blob-result-section";
 export default function UploadSection() {
   const inputFileRef = useRef<HTMLInputElement>(null);
   const [blob, setBlob] = useState<PutBlobResult | null>(null);
-  const [loading, setLoading] = useState(false);
+  const [isPending, startTransition] = useTransition();
+
+  const handleSubmmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    startTransition(async () => {
+      if (!inputFileRef.current?.files) {
+        throw new Error("No file selected");
+      }
+
+      const file = inputFileRef.current.files[0];
+
+      const response = await fetch(`/api/avatar/upload?filename=${file.name}`, {
+        method: "POST",
+        body: file,
+      });
+
+      if (response.ok) {
+        const newBlob = (await response.json()) as PutBlobResult;
+        setBlob(newBlob);
+      } else {
+        console.error("Upload failed");
+      }
+    });
+  };
 
   return (
     <>
-      <form
-        onSubmit={async (event) => {
-          event.preventDefault();
-          setLoading(true);
-
-          if (!inputFileRef.current?.files) {
-            setLoading(false);
-            throw new Error("No file selected");
-          }
-
-          const file = inputFileRef.current.files[0];
-
-          const response = await fetch(
-            `/api/avatar/upload?filename=${file.name}`,
-            {
-              method: "POST",
-              body: file,
-            }
-          );
-
-          if (response.ok) {
-            const newBlob = (await response.json()) as PutBlobResult;
-            setBlob(newBlob);
-          } else {
-            console.error("Upload failed");
-          }
-          setLoading(false);
-        }}
-      >
+      <form onSubmit={handleSubmmit}>
         <UploadContainer inputFileRef={inputFileRef} />
         <div className="flex justify-center my-5">
-          <button type="submit" disabled={loading} className="px-4 py-2 bg-blue-500 text-white rounded">
-          Upload
-        </button>
+          <button
+            type="submit"
+            disabled={isPending}
+            className="px-4 py-2 bg-blue-500 text-white rounded"
+          >
+            {isPending ? "Uploading..." : "Upload"}
+          </button>
         </div>
-        
       </form>
-      {loading ? (
+      {isPending ? (
         <p className="animate-pulse h-5 w-full bg-gray-200 rounded"></p>
       ) : (
         <BlobResultSection blob={blob} />
